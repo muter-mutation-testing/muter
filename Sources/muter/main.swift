438735b8ed8b1ca2ced6763c020e023c99f07cc9
SeanROlszewski <sean.r.olszewski@gmail.com>
2018-11-11 18:40:52 -0500

    muter can introduce mutation into a codebase

--- Sources/muter/main.swift
+++ Sources/muter/main.swift
@@ -19,15 +19,10 @@ xcodebuild \
 test
 """
 
-
-
-switch CommandLine.argc {
-case 2:
-    let path = CommandLine.arguments[1]
-
+func runTestSuite() {
     if #available(OSX 10.13, *) {
         let url = URL(fileURLWithPath: "/usr/bin/xcodebuild")
-        try Process.run(url, arguments: [
+        try! Process.run(url, arguments: [
             "-verbose",
             "-project",
             "/Users/seandorian/Code/Swift/muter/Tests/muterTests/fixtures/MuterExampleTestSuite/MuterExampleTestSuite.xcodeproj",
@@ -38,22 +33,53 @@ case 2:
             "-destination",
             "platform=iOS Simulator,name=iPhone 6",
             "test",
-        ]) { (process) in
-            print("process finished running: \(process.isRunning)")
-        }.waitUntilExit()
-        
+            ]) { (process) in
+                print("process finished running: \(!process.isRunning) \(process.terminationStatus)")
+                
+            }.waitUntilExit()
     } else {
-        // Fallback on earlier versions
+        print("muter is only supported on macOS 10.13 and higher")
+        exit(1)
     }
+}
+
+func copyOriginalSourceCode(fromFileAt path: String) {
+    try! FileManager.default.createDirectory(atPath: "/Users/seandorian/Code/Swift/muter/Tests/muterTests/fixtures/muter_tmp/MuterExampleTestSuite/MuterExampleTestSuite", withIntermediateDirectories: true, attributes: nil)
+    let swapFile = "/Users/seandorian/Code/Swift/muter/Tests/muterTests/fixtures/muter_tmp/MuterExampleTestSuite/MuterExampleTestSuite/Module.swift"
+    
+    
+    let sourceCode = try! FileParser().load(path: path)
+    try! sourceCode.description.write(toFile: swapFile, atomically: true, encoding: .utf8)
+}
+
+func mutateSourceCode(inFileAt path: String) {
+    let sourceCode = try! FileParser().load(path: path)
+    
+    let mutatedSourceCode = ConditionalBoundaryMutation().mutate(source: sourceCode)
+    
+    try! mutatedSourceCode.description.write(toFile: path, atomically: true, encoding: .utf8)
+}
+
+func restoreSourceCode(forFileAt path: String) {
+    let swapFile = "/Users/seandorian/Code/Swift/muter/Tests/muterTests/fixtures/muter_tmp/MuterExampleTestSuite/MuterExampleTestSuite/Module.swift"
+    
+    
+    let sourceCode = try! FileParser().load(path: swapFile)
+    try! sourceCode.description.write(toFile: path, atomically: true, encoding: .utf8)
+}
+
+switch CommandLine.argc {
+case 2:
+//    let path = CommandLine.arguments[1]
+    let path = "/Users/seandorian/Code/Swift/muter/Tests/muterTests/fixtures/MuterExampleTestSuite/MuterExampleTestSuite/Module.swift"
+    
+    copyOriginalSourceCode(fromFileAt: path)
+    mutateSourceCode(inFileAt: path)
+    runTestSuite()
+    restoreSourceCode(forFileAt: path)
     
     exit(0)
 default:
     printUsageStatement()
     exit(1)
 }
-
-
-
-
-
-
