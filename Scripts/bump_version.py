c811de483bd6a267feaca6cfe725abd372e1052e
SeanROlszewski <sean.r.olszewski@gmail.com>
2019-01-20 18:47:00 -0500

    Initial version of the deployment script

---
+++ Scripts/bump_version.py
@@ -0,0 +1,28 @@
+from tempfile import mkstemp
+from shutil import move
+from os import fdopen, remove
+import sys
+
+def replace(file_path, new_url, new_sha):
+    #Create temp file
+    fh, abs_path = mkstemp()
+    new_lines = []
+    with open(file_path) as file:
+        new_lines = file.readlines()
+
+        new_lines[3] = "\turl \"%s\"\n" % (new_url) 
+        new_lines[4] = "\tsha256 \"%s\"\n" % (new_sha)
+
+    with open(file_path, 'w') as file:
+        for line in new_lines:
+            file.write("%s" % (line))
+
+
+if len(sys.argv) == 3:
+    
+    replace("./homebrew-formulae/Formula/muter.rb", 
+            "https://github.com/SeanROlszewski/muter/archive/%s.zip" % (sys.argv[1]),
+            sys.argv[2])
+
+else:
+    print "usage: bump_version new_version sha256_hash_of_new_version"
---
+++ Scripts/shipIt.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+if [ "$1" == "" ]; then
+    echo "Provide a version string in order to release a new build"
+    exit
+else 
+
+    echo "Bumping version number inside of version.swift..."
+    echo "let version = \"$1\"" > ./Sources/muter/version.swift
+
+    echo "Committing new version..."
+    git add ./Sources/muter/version.swift
+    git commit -m "Bump version to $1"
+    git tag "$1"
+    git push origin head
+    git push origin head --tags
+
+    echo "Generating SHA256 hash of the new version..."
+    curl -L "https://github.com/SeanROlszewski/muter/archive/$1.zip" -o new_muter_version.zip
+    SHA=$(shasum -a 256 "./new_muter_version.zip" | cut -d " " -f 1) 
+    rm new_muter_version.zip
+
+    echo "Updating Homebrew formula..."
+    python ./Scripts/bump_version.py $1 $SHA
+
+    echo "Uninstalling old version..."
+    make uninstall
+    brew uninstall muter
+
+    cd ./homebrew-formulae/Formula/  
+
+    echo "Testing new Homebrew formula..."
+    brew install --build-from-source ./muter.rb
+
+    # git add ./muter.rb
+    # git commit -m "Update Muter's formula for its $1 release"
+
+
+    
+fi
