d138db4a6cb52a2c79c1c2ac729df0882252adec
Rafael <raka.cdc@gmail.com>
2019-02-09 13:50:31 -0200

    Extract configuration extention

--- Tests/configurationParsingSpec.swift
+++ Tests/configurationParsingSpec.swift
@@ -32,14 +32,3 @@ class ConfigurationParsingSpec: QuickSpec {
         }
     }
 }
-
-extension MuterConfiguration {
-    static func fromFixture(at path: String) -> MuterConfiguration? {
-
-        guard let data = FileManager.default.contents(atPath: path),
-            let configuration = try? JSONDecoder().decode(MuterConfiguration.self, from: data) else {
-                fatalError("Unable to load a valid Muter configuration file from \(path)")
-        }
-        return configuration
-    }
-}
