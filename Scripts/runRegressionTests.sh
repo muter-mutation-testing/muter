dd6013060f9ef280509b17f0a20df86853d311f4
SeanROlszewski <sean.r.olszewski@gmail.com>
2019-02-09 20:34:56 -0500

    Update regression test scripts to use command line flag for json report output

--- Scripts/runRegressionTests.sh
+++ Scripts/runRegressionTests.sh
@@ -3,7 +3,7 @@ echo "🦕🦕🦕🦕🦕🦕🦕🦕 Regression Testing has started 🦕🦕�
 
 echo "Running Regression Test on Bon Mot..."
 cd ./Repositories/BonMot
-../../.build/x86_64-apple-macosx10.10/debug/muter
+../../.build/x86_64-apple-macosx10.10/debug/muter --output-json
 cp ./muterReport.json ../../Tests/bonmot_regression_test_output.json
 cd ../..
 make test
