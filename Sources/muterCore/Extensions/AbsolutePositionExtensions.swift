23f743660fea69d50581b993d00a5214067c39f0
SeanROlszewski <sean.r.olszewski@gmail.com>
2019-02-09 22:06:50 -0500

    Style changes

--- Sources/muterCore/Extensions/AbsolutePositionExtensions.swift
+++ Sources/muterCore/Extensions/AbsolutePositionExtensions.swift
@@ -13,16 +13,20 @@ extension AbsolutePosition: Codable {
         case column
         case utf8Offset
     }
+    
     public init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: Keys.self)
-        let  line = try container.decode(Int.self, forKey: .line)
+        
+        let line = try container.decode(Int.self, forKey: .line)
         let column = try container.decode(Int.self, forKey: .column)
         let utf8Offset = try container.decode(Int.self, forKey: .utf8Offset)
+        
         self.init(line: line, column: column, utf8Offset: utf8Offset)
     }
     
     public func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: Keys.self)
+        
         try container.encode(line, forKey: .line)
         try container.encode(column, forKey: .column)
         try container.encode(utf8Offset, forKey: .utf8Offset)
