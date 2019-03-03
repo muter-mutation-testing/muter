dafe3b9aac09eee3ce548cd6f440ed44da540acf
SeanROlszewski <sean.r.olszewski@gmail.com>
2019-03-02 22:46:11 -0500

    Move all codebases for acceptance & regression testing into Repositories
    [closes #62]

--- Scripts/runAcceptanceTests.sh
+++ Scripts/runAcceptanceTests.sh
@@ -4,21 +4,19 @@ echo "📴📴📴📴📴📴📴 Acceptance Testing has started 📴📴📴�
 echo "Setting up environment for testing..."
 
 echo "Running Muter on a codebase with a test suite..."
-cd ./ExampleApp
-../.build/x86_64-apple-macosx10.10/debug/muter > ../AcceptanceTests/muters_output.txt 
-
-cd ..
+cd ./Repositories/ExampleApp
+../../.build/x86_64-apple-macosx10.10/debug/muter > ../../AcceptanceTests/muters_output.txt
+cd ../..
 
 echo "Running Muter on an empty example codebase..."
-cd ./EmptyExampleApp
-../.build/x86_64-apple-macosx10.10/debug/muter > ../AcceptanceTests/muters_empty_state_output.txt
-
-cd ..
+cd ./Repositories/EmptyExampleApp
+../../.build/x86_64-apple-macosx10.10/debug/muter > ../../AcceptanceTests/muters_empty_state_output.txt
+cd ../..
 
 echo "Running Muter on an example test suite that fails..."
-cd ./ProjectWithFailures
-../.build/x86_64-apple-macosx10.10/debug/muter > ../AcceptanceTests/muters_aborted_testing_output.txt
-cd ..
+cd ./Repositories/ProjectWithFailures
+../../.build/x86_64-apple-macosx10.10/debug/muter > ../../AcceptanceTests/muters_aborted_testing_output.txt
+cd ../..
 
 echo "Running tests..."
 swift package generate-xcodeproj
--- Scripts/runRegressionTests.sh
+++ Scripts/runRegressionTests.sh
@@ -1,20 +1,18 @@
 #!/bin/sh
+
 echo "🦕🦕🦕🦕🦕🦕🦕🦕 Regression Testing has started 🦕🦕🦕🦕🦕🦕🦕🦕"
 
 echo "Running Regression Test on Bon Mot..."
 cd ./Repositories/BonMot
 ../../.build/x86_64-apple-macosx10.10/debug/muter --output-json
 cp ./muterReport.json ../../RegressionTests/bonmot_regression_test_output.json
-
 cd ../..
 
 echo "Running Regression Test on Parser Combinator..."
 cd ./Repositories/FFCParserCombinator
-
 swift package generate-xcodeproj
 ../../.build/x86_64-apple-macosx10.10/debug/muter --output-json
 cp ./muterReport.json ../../RegressionTests/parsercombinator_regression_test_output.json
-
 cd ../..
 
 swift package generate-xcodeproj
