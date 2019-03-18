68963ec6ad3eff2dc5bfdad478a1e3d724646daa
SeanROlszewski <sean.r.olszewski@gmail.com>
2019-03-18 14:00:47 -0400

    Speed up acceptance tests
     - Refactor acceptance tests to accommodate changes to the example app, as well as to make them less prone to false positives and negatives

--- Repositories/ExampleApp/ExampleApp/Module.swift
+++ Repositories/ExampleApp/ExampleApp/Module.swift
@@ -11,19 +11,3 @@ func areAlsoEqual(_ a: Int, and b: Int) -> Bool {
 func areNotEqual(_ a: Int, and b: Int) -> Bool {
         return a != b
 }
-
-func isGreaterThanOrEqualTo(_ a: Int, and b: Int) -> Bool {
-        return a >= b
-}
-
-func isLessThanOrEqualTo(_ a: Int, and b: Int) -> Bool {
-        return a <= b
-}
-
-func isGreaterTo(_ a: Int, and b: Int) -> Bool {
-        return a > b
-}
-
-func isLessTo(_ a: Int, and b: Int) -> Bool {
-        return a < b
-}
