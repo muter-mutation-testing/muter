8ae3b83cbf7cb033d156fd604617614ca62570ae
Rafael Machado <raka.cdc@gmail.com>
2019-08-01 21:11:31 -0300

    Immutable version of removeSubrange

--- Sources/muterCore/Extensions/StringExtensions.swift
+++ Sources/muterCore/Extensions/StringExtensions.swift
@@ -4,6 +4,13 @@ extension String {
     func repeated(_ times: Int) -> String {
         return (0 ..< times).reduce("") { current, _ in current + "\(self)"}
     }
+    
+    func removingSubrange(_ bounds: Range<String.Index>?) -> String {
+        guard let bounds = bounds else { return self }
+        var result = self
+        result.removeSubrange(bounds)
+        return result
+    }
 
     var trimmed: String {
         return trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
