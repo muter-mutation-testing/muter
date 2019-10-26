0d56be77ca6e0d927ffb89227b388c35da858342
Rafael Machado <raka.cdc@gmail.com>
2019-10-25 23:54:20 -0300

    Add concurrency project to regression tests

---
+++ Repositories/ProjectWithConcurrency/ProjectWithConcurrency/main.swift
@@ -0,0 +1,39 @@
+import Foundation
+
+private let semaphore = DispatchSemaphore(value: 1)
+private let conditionLock = NSConditionLock(condition: 0)
+private let lock = NSRecursiveLock()
+private let condition = NSCondition()
+
+private func semaphoreLock(block: () -> Void) {
+    defer{ semaphore.signal() }
+    semaphore.wait()
+    block()
+}
+
+private func recursiveLock(block: () -> Void) {
+    defer { lock.unlock() }
+    lock.lock()
+    block()
+}
+
+private func nsCondition(block: () -> Void) {
+    defer { condition.signal() }
+    condition.wait()
+    block()
+}
+
+private func nsConditionLock(block: () -> Void) {
+    defer { conditionLock.unlock(withCondition: 1) }
+    conditionLock.lock(whenCondition: 1)
+    block()
+}
+
+private func sync(block: () -> Int) -> Int {
+    let semaphore = DispatchSemaphore(value: 2)
+    semaphore.wait()
+    let value = block()
+    semaphore.signal()
+    
+    return value
+}
