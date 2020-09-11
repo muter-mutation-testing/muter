28d7e73abb103c0cfa9b536bafc16f2bf7c07d01
Zev Eisenberg <zev.eisenberg@nytimes.com>
2020-09-10 22:10:53 -0400

    Add comments.

--- Sources/muterCore/Configuration/SourceFileInfo.swift
+++ Sources/muterCore/Configuration/SourceFileInfo.swift
@@ -1,4 +1,9 @@
+/// The full path to a file on disk, and its complete contents as a string.
 struct SourceFileInfo {
+
+    /// The path to the file on disk.
     let file: String
+
+    /// The full contents of the file.
     let source: String
 }
