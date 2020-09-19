2b5cb372fcdec107d9905ded949912357ff72f5c
Rafael Machado <raka.cdc@gmail.com>
2020-09-19 18:16:07 -0300

    Add missing changes

--- TestingExtensions/fixtures/MutationExamples/SideEffect/sampleWithImplicitReturn.swift
+++ TestingExtensions/fixtures/MutationExamples/SideEffect/sampleWithImplicitReturn.swift
@@ -6,10 +6,18 @@ func mutateMe2() -> Void {
     bar()
 }
 
+func mutateMe3() ->  () {
+    bar()
+}
+
 func bar() { }
 
 func ignoreMe() -> Int {
     returnInt()
+}
+
+func ignoreMe2() -> Int {
+    return returnInt()
 }
 
 func returnInt() -> Int {
