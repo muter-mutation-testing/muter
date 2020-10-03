3e9c0a14e44381e98b6aa84ecb978e8ecddc3761
Rafael Machado <raka.cdc@gmail.com>
2020-10-02 22:49:03 -0300

    Extract plain text reporter

--- Sources/muterCore/TestReporting/Reporter.swift
+++ Sources/muterCore/TestReporting/Reporter.swift
@@ -31,45 +31,6 @@ extension Reporter {
     }
 }
 
-private extension Reporter {
-    func textReport(from outcomes: [MutationTestOutcome]) -> String {
-        let report = MuterTestReport(from: outcomes)
-        
-        let finishedRunningMessage = "Muter finished running!\n\nHere's your test report:\n\n"
-        let appliedMutationsMessage = """
-        --------------------------
-        Applied Mutation Operators
-        --------------------------
-        
-        These are all of the ways that Muter introduced changes into your code.
-        
-        In total, Muter introduced \(report.totalAppliedMutationOperators) mutants in \(report.fileReports.count) files.
-        
-        \(generateAppliedMutationOperatorsCLITable(from: report.fileReports).description)
-        
-        
-        """
-        
-        let coloredGlobalScore = coloredMutationScore(for: report.globalMutationScore, appliedTo: "\(report.globalMutationScore)%")
-        let mutationScoreMessage = "Mutation Score of Test Suite: ".bold + "\(coloredGlobalScore)"
-        let mutationScoresMessage = """
-        --------------------
-        Mutation Test Scores
-        --------------------
-        
-        These are the mutation scores for your test suite, as well as the files that had mutants introduced into them.
-        
-        Mutation scores ignore build errors.
-        
-        Of the \(report.totalAppliedMutationOperators) mutants introduced into your code, your test suite killed \(report.numberOfKilledMutants).
-        \(mutationScoreMessage)
-        
-        \(generateMutationScoresCLITable(from: report.fileReports).description)
-        """
-        
-        return finishedRunningMessage + appliedMutationsMessage + mutationScoresMessage
-    }
-    
 
 
 }
