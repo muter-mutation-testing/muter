f29c8b4fb3a21ad8db92852752a9edf0b63dd474
Zev Eisenberg <zev.eisenberg@nytimes.com>
2020-11-02 10:57:27 -0500

    Use a defer to guard against future early returns.

--- Sources/muterCore/MutationTesting/MutationTestingIODelegate.swift
+++ Sources/muterCore/MutationTesting/MutationTestingIODelegate.swift
@@ -23,11 +23,11 @@ struct MutationTestingDelegate: MutationTestingIODelegate {
                       savingResultsIntoFileNamed fileName: String) -> (outcome: TestSuiteOutcome, testLog: String) {
         do {
             let (testProcessFileHandle, testLogUrl) = try fileHandle(for: fileName)
+            defer { testProcessFileHandle.closeFile() }
 
             let process = try testProcess(with: configuration, and: testProcessFileHandle)
             try process.run()
             process.waitUntilExit()
-            testProcessFileHandle.closeFile()
 
             let contents = try String(contentsOf: testLogUrl)
 
