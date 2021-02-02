4f5e9c2d474decf456b5160719e233b0813ec257
Rafael Machado <raka.cdc@gmail.com>
2021-02-02 13:14:06 -0300

    Extract resources and refactor report

--- Sources/muterCore/TestReporting/HTML/HTMLReporter.swift
+++ Sources/muterCore/TestReporting/HTML/HTMLReporter.swift
@@ -25,7 +25,15 @@ private func htmlReport(
     _ now: Now,
     _ testReport: MuterTestReport
 ) -> String {
-    """
+    let normalizeCSS = Bundle.resource(named: "normalize", ofType: "css")
+    let reportCSS = Bundle.resource(named: "report", ofType: "css")
+    let css = normalizeCSS + reportCSS
+
+    let javascript = Bundle.resource(named: "javascript", ofType: "js")
+
+    let muterLogo = Bundle.resource(named: "muterLogo", ofType: "svg")
+
+    return """
     <!DOCTYPE html>
     <html lang="en">
     <head>
@@ -127,7 +135,7 @@ private extension Array where Element == MuterTestReport.FileReport {
         }.joined(separator: "\n")
 
         return """
-        <table id=\"mutation-operators-per-file\">
+        <table id="mutation-operators-per-file">
             \(thead)
             <tbody>
             \(tbody)
@@ -163,7 +171,7 @@ private extension Array where Element == MuterTestReport.FileReport {
         }.joined(separator: "\n")
 
         return """
-        <table id=\"applied-operators\">
+        <table id="applied-operators">
             \(thead)
             <tbody>
             \(tbody)
@@ -185,7 +193,7 @@ private extension MutationOperator.Id {
 
 private extension MuterTestReport.AppliedMutationOperator {
     var diff: String {
-        var diff = ""
+        let diff: String
         if mutationPoint.mutationOperatorId == .removeSideEffects {
             diff = """
             <span class="snapshot-before">\(mutationSnapshot.before)</span>
@@ -208,41 +216,26 @@ private extension MuterTestReport.AppliedMutationOperator {
 
 private extension TestSuiteOutcome {
     var asIcon: String {
+        let icon: String
         switch (self) {
         case .passed:
-            return """
-                    <svg class="failed" role="img" viewBox="0 0 72 72">
-                        <title>\(asMutationTestOutcome)</title>
-                        <path fill="#ea5a47" d="M58.14 21.78l-7.76-8.013-14.29 14.22-14.22-14.22-8.013 8.013L28.217 36l-14.36 14.22 8.014 8.013 14.22-14.22 14.29 14.22 7.76-8.013L43.921 36z"/>
-                        <path fill="none" stroke="#000" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="2" d="M58.14 21.78l-7.76-8.013-14.29 14.22-14.22-14.22-8.013 8.013L28.207 36l-14.35 14.22 8.014 8.013 14.22-14.22 14.29 14.22 7.76-8.013L43.921 36z"/>
-                    </svg>
-                    """
+            icon = Bundle.resource(
+                named: "testPassed",
+                ofType: "svg"
+            )
         case .failed, .runtimeError:
-            return """
-                    <svg class="passed" role="img" viewBox="0 0 72 72">
-                        <title>\(asMutationTestOutcome)</title>
-                        <path fill="#b1cc33" d="m61.5 23.3-8.013-8.013-25.71 25.71-9.26-9.26-8.013 8.013 17.42 17.44z"/>
-                        <path fill="none" stroke="#000" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="2" d="M10.5 39.76L27.92 57.2 61.5 23.31l-8.013-8.013-25.71 25.71-9.26-9.26z"/>
-                    </svg>
-                    """
+            icon = Bundle.resource(
+                named: "testFailed",
+                ofType: "svg"
+            )
         case .buildError:
-            return """
-                    <svg class="build-error" role="img" viewBox="0 0 72 72">
-                        <title>\(asMutationTestOutcome)</title>
-                        <path fill="#FFF" d="M40.526 16.92a14.934 14.934 0 00-4.336-.645h-.03a15.731 15.731 0 00-3.121.324 15.412 15.412 0 00-9.095 5.832 17.022 17.022 0 00-3.267 7.59 17.404 17.404 0 004.02 14.42l.112.126c.884.996 2.094 2.362 2.094 3.814l-.019 4.178a.838.838 0 01-.837.837 25.063 25.063 0 005.873 2.124c3.192.72 6.514.629 9.662-.263 2.693-12.712 1.982-25.99-1.056-38.337z"/>
-                        <path fill="#D0CFCE" d="M46.98 20.812a15.5 15.5 0 00-6.454-3.893c3.038 12.348 3.75 25.626 1.054 38.332a22.514 22.514 0 004.69-1.902.846.846 0 01-.85-.825l-.057-4.14c0-1.383 1.173-2.673 2.028-3.616.121-.134.237-.26.34-.38 5.907-6.861 5.583-17.1-.746-23.575h-.005z"/>
-                        <path fill="#3F3F3F" d="M29.37 35.24a4 4 0 100 8 4 4 0 000-8zM43.062 35.24a4 4 0 100 8 4 4 0 000-8z"/>
-                        <path fill="#FFF" d="M53.015 20.665a460.015 460.015 0 012.416-2.39s1.943 1.84 3.27 1.443c1.373-.41 2.156-1.808 2.035-2.954-.167-1.588-2.11-3.04-4.438-2.556.406-1.513.068-3.062-.933-3.995-1.654-1.54-4.078-.479-4.99.918-1.22 1.869 1.574 3.663 1.574 3.663a410.45 410.45 0 00-2.177 2.151M19.678 46.908c-1.549 1.54-2.6 2.58-2.92 2.89-.655-1.302-2.132-1.943-3.458-1.546-1.374.411-2.157 1.808-2.036 2.954.168 1.589 2.11 3.04 4.438 2.556-.405 1.513-.068 3.062.933 3.995 1.654 1.54 4.33.614 4.99-.918.47-1.088.193-2.297-1.438-3.505.26-.252 1.005-.99 2.11-2.087M19.113 20.792a489.893 489.893 0 00-2.543-2.516s-1.944 1.839-3.27 1.442c-1.374-.411-2.157-1.808-2.036-2.954.168-1.589 2.11-3.04 4.438-2.556-.405-1.513-.068-3.062.933-3.995 1.654-1.54 4.078-.479 4.99.918 1.22 1.869-1.573 3.663-1.573 3.663.286.277 1.157 1.14 2.447 2.421M52.497 47.082a535.186 535.186 0 002.744 2.716c.655-1.302 2.133-1.943 3.459-1.546 1.374.411 2.157 1.808 2.036 2.954-.168 1.589-2.11 3.04-4.438 2.556.405 1.513.068 3.062-.933 3.995-1.654 1.54-4.33.614-4.99-.918-.47-1.088-.193-2.297 1.438-3.505-.258-.25-.991-.975-2.078-2.055"/>
-                        <g fill="none" stroke="#000" stroke-width="2">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M46.255 52.473l-.058-4.14c0-1.215 1.381-2.543 2.161-3.435A18.046 18.046 0 0052.79 32.97c0-9.716-7.45-17.594-16.63-17.575a16.518 16.518 0 00-3.288.341c-6.608 1.355-11.874 7.083-13.022 14.106a18.22 18.22 0 004.224 15.11c.768.87 1.995 2.195 1.995 3.385l-.018 4.173M29.371 51.081v4.004M34.157 51.973v4.005M43.063 51.081v4.004M38.72 51.973v4.005"/>
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M33.702 48.742l2.456-4.899 2.936 4.899"/>
-                        <circle cx="29.371" cy="39.2" r="4.188" stroke-miterlimit="10"/>
-                        <circle cx="43.063" cy="39.2" r="4.188" stroke-miterlimit="10"/>
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M53.015 20.665a460.015 460.015 0 012.416-2.39s1.943 1.84 3.27 1.443c1.373-.41 2.156-1.808 2.035-2.954-.167-1.588-2.11-3.04-4.438-2.556.406-1.513.068-3.062-.933-3.995-1.654-1.54-4.078-.479-4.99.918-1.22 1.869 1.574 3.663 1.574 3.663a410.45 410.45 0 00-2.177 2.151M19.678 46.908c-1.549 1.54-2.6 2.58-2.92 2.89-.655-1.302-2.132-1.943-3.458-1.546-1.374.411-2.157 1.808-2.036 2.954.168 1.589 2.11 3.04 4.438 2.556-.405 1.513-.068 3.062.933 3.995 1.654 1.54 4.33.614 4.99-.918.47-1.088.193-2.297-1.438-3.505.26-.252 1.005-.99 2.11-2.087M19.113 20.792a489.893 489.893 0 00-2.543-2.516s-1.944 1.839-3.27 1.442c-1.374-.411-2.157-1.808-2.036-2.954.168-1.589 2.11-3.04 4.438-2.556-.405-1.513-.068-3.062.933-3.995 1.654-1.54 4.078-.479 4.99.918 1.22 1.869-1.573 3.663-1.573 3.663.286.277 1.157 1.14 2.447 2.421M52.497 47.082a535.186 535.186 0 002.744 2.716c.655-1.302 2.133-1.943 3.459-1.546 1.374.411 2.157 1.808 2.036 2.954-.168 1.589-2.11 3.04-4.438 2.556.405 1.513.068 3.062-.933 3.995-1.654 1.54-4.33.614-4.99-.918-.47-1.088-.193-2.297 1.438-3.505-.258-.25-.991-.975-2.078-2.055"/>
-                      </g>
-                    </svg>
-                    """
+            icon = Bundle.resource(
+                named: "testBuildError",
+                ofType: "svg"
+            )
         }
+        
+        return icon.replacingOccurrences(of: "$title$", with: asMutationTestOutcome)
     }
 }
 
@@ -260,5 +253,14 @@ private func coloredMutationScore(for score: Int) -> String {
     case 26...50: return "#ce9400"
     case 51...75: return "#92b300"
     default: return "#51a100"
+    }
+}
+
+extension Bundle {
+    static func resource(named: String, ofType type: String) -> String {
+        Bundle.module.path(forResource: named, ofType: type)
+            .flatMap {
+                try? String(contentsOfFile: $0)
+            } ?? ""
     }
 }
