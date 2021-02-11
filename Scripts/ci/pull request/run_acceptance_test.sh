61cd9ae50541306015a82732c253650690c47edf
Rafael Machado <raka.cdc@gmail.com>
2021-02-11 19:30:58 -0300

    Update scripts

--- Scripts/ci/pull request/run_acceptance_test.sh
+++ Scripts/ci/pull request/run_acceptance_test.sh
@@ -2,22 +2,4 @@
 
 set -o pipefail
 
-source ./Scripts/test_utils.sh
-
-function passed() {
-  XCRESULT_PATH=( ./.derivedData/Logs/Test/*.xcresult )
-  XCRESULT_PATH="${XCRESULT_PATH[0]}"
-  
-  envman add --key XCRESULT_PATH --value "$XCRESULT_PATH"
-
-  exit 0;
-}
-
-builddir=$(xcodebuild -showBuildSettings | grep "TOOLCHAIN_DIR" | grep -oEi "\/.*" | tail -1)
-
-xcodebuild -scheme muter \
-           -enableCodeCoverage YES \
-           -derivedDataPath ./.derivedData \
-           -only-testing:muterAcceptanceTests \
-           test \
-           LD_RUNPATH_SEARCH_PATHS="$builddir/usr/lib/swift/macosx" 2>&1 | parseXcodebuildOutput | xcpretty && passed
+make acceptance-test
