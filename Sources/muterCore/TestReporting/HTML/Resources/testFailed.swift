9fd32d1480dd419bcae942a720b4099432582cbb
Rafael Machado <raka.cdc@gmail.com>
2021-11-03 11:44:29 -0300

    Stop using bundle resources

--- Sources/muterCore/TestReporting/HTML/Resources/testFailed.swift
+++ Sources/muterCore/TestReporting/HTML/Resources/testFailed.swift
@@ -1,5 +1,8 @@
+let testFailed =
+"""
 <svg class="passed" role="img" viewBox="0 0 72 72">
     <title>$title$</title>
     <path fill="#b1cc33" d="m61.5 23.3-8.013-8.013-25.71 25.71-9.26-9.26-8.013 8.013 17.42 17.44z"/>
     <path fill="none" stroke="#000" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="2" d="M10.5 39.76L27.92 57.2 61.5 23.31l-8.013-8.013-25.71 25.71-9.26-9.26z"/>
 </svg>
+"""
