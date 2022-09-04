342f4945fc3677c636ceb1c9f38dced20985af0d
Rafael Machado <raka.cdc@gmail.com>
2022-09-04 16:21:49 -0300

    refactor config test double

--- Tests/Extensions/MuterConfigurationExtensions.swift
+++ Tests/Extensions/MuterConfigurationExtensions.swift
@@ -5,7 +5,7 @@ public extension MuterConfiguration {
     static func fromFixture(at path: String) -> MuterConfiguration? {
 
         guard let data = FileManager.default.contents(atPath: path),
-            let configuration = try? JSONDecoder().decode(MuterConfiguration.self, from: data) else {
+            let configuration = try? MuterConfiguration.make(from: data) else {
                 fatalError("Unable to load a valid Muter configuration file from \(path)")
         }
         return configuration
