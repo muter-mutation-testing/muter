9f67ca646fbbc13fb542b30f71599f59dc82157b
Rafael Machado <raka.cdc@gmail.com>
2023-06-23 12:43:40 -0300

    wip

--- Sources/muterCore/SourceCodeOperations/fileOperations.swift
+++ Sources/muterCore/SourceCodeOperations/fileOperations.swift
@@ -1,21 +1,14 @@
 import Foundation
 import SwiftSyntax
-import SwiftParser
 import SwiftSyntaxParser
 
 // MARK: - Source Code
 
 func sourceCode(fromFileAt path: String) -> SourceCodeInfo? {
-    guard let fileContents = FileManager.default.contents(atPath: path),
-          let sourceFile = String(data: fileContents, encoding: .utf8) else {
-        return nil
-    }
-
-    let sourceCode = Parser.parse(source: sourceFile)
-    return SourceCodeInfo(
-        path: path,
-        code: sourceCode
-    )
+    let url = URL(fileURLWithPath: path)
+    return (try? SyntaxParser.parse(url))
+        .map { (path: url.absoluteString, code: $0) }
+        .map(SourceCodeInfo.init)
 }
 
 // MARK: - Logging Directory
@@ -23,12 +16,12 @@ func createLoggingDirectory(in directory: String,
                             fileManager: FileSystemManager = FileManager.default,
                             locale: Locale = .autoupdatingCurrent,
                             timestamp: () -> Date = Date.init) -> String {
-    
+
     let formatter = DateFormatter()
     formatter.locale = locale
     formatter.dateStyle = .medium
     formatter.timeStyle = .short
-    
+
     let loggingDirectory = "\(directory)/muter_logs/\(formatter.string(from: timestamp()))"
     try! fileManager.createDirectory(atPath: loggingDirectory, withIntermediateDirectories: true, attributes: nil)
     return loggingDirectory
