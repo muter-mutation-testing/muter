be392a5a48a6db46b30c816e0d64562116d2a8b0
gsl-anthonymerle <anthony.merle@groupeseloger.com>
2024-01-25 11:56:23 +0100

    Make XCTestRun.updateEnvironmentVariable handle xctestplan configuration

--- Sources/muterCore/BuildSystems/Xcode/XCTestRun.swift
+++ Sources/muterCore/BuildSystems/Xcode/XCTestRun.swift
@@ -11,18 +11,48 @@ struct XCTestRun: Equatable {
         setting key: String
     ) -> [String: AnyHashable] {
         var copy = plist
+        let testConfigurationsKey = "TestConfigurations"
+        let testTargetsKey = "TestTargets"
         let environmentVariablesKey = "EnvironmentVariables"
 
-        for (plistEntry, plistValue) in copy {
-            if var testConfiguration = plistValue as? [String: AnyHashable],
-               testConfiguration.keys.contains(environmentVariablesKey),
-               var allEnvironmentVariables = testConfiguration[environmentVariablesKey] as? [String: AnyHashable] {
-                allEnvironmentVariables[key] = isMuterRunningValue
-                allEnvironmentVariables[isMuterRunningKey] = isMuterRunningValue
+        if let testConfigurations = copy[testConfigurationsKey] as? [AnyHashable] {
+            // Using Xcode's TestPlans
+            let newTestConfigurations = testConfigurations.map { testConfiguration in
+                if var newTestConfiguration = testConfiguration as? [String: AnyHashable],
+                   let testTargets = newTestConfiguration[testTargetsKey] as? [AnyHashable] {
+                    let newTestTargets = testTargets.map { testTarget in
+                        if var newTestTarget = testTarget as? [String: AnyHashable],
+                           var allEnvironmentVariables = newTestTarget[environmentVariablesKey] as? [String: AnyHashable] {
+                            allEnvironmentVariables[key] = isMuterRunningValue
+                            allEnvironmentVariables[isMuterRunningKey] = isMuterRunningValue
 
-                testConfiguration[environmentVariablesKey] = allEnvironmentVariables
+                            newTestTarget[environmentVariablesKey] = allEnvironmentVariables
 
-                copy[plistEntry] = testConfiguration
+                            return newTestTarget as AnyHashable
+                        }
+                        return testTarget
+                    }
+
+                    newTestConfiguration[testTargetsKey] = newTestTargets
+
+                    return newTestConfiguration as AnyHashable
+                }
+                return testConfiguration
+            }
+
+            copy[testConfigurationsKey] = newTestConfigurations
+        } else { // Legacy Tests config
+            for (plistEntry, plistValue) in copy {
+                if var testConfiguration = plistValue as? [String: AnyHashable],
+                   testConfiguration.keys.contains(environmentVariablesKey),
+                   var allEnvironmentVariables = testConfiguration[environmentVariablesKey] as? [String: AnyHashable] {
+                    allEnvironmentVariables[key] = isMuterRunningValue
+                    allEnvironmentVariables[isMuterRunningKey] = isMuterRunningValue
+
+                    testConfiguration[environmentVariablesKey] = allEnvironmentVariables
+
+                    copy[plistEntry] = testConfiguration
+                }
             }
         }
 
