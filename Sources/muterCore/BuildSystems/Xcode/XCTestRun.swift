ae229a4568a7af46795debc895e956e23d195e65
gsl-anthonymerle <anthony.merle@groupeseloger.com>
2024-01-25 15:51:22 +0100

    Refactored XCTestRun.updateEnvironmentVariables to avoid code duplication

--- Sources/muterCore/BuildSystems/Xcode/XCTestRun.swift
+++ Sources/muterCore/BuildSystems/Xcode/XCTestRun.swift
@@ -13,22 +13,18 @@ struct XCTestRun: Equatable {
         var copy = plist
         let testConfigurationsKey = "TestConfigurations"
         let testTargetsKey = "TestTargets"
-        let environmentVariablesKey = "EnvironmentVariables"
 
         if let testConfigurations = copy[testConfigurationsKey] as? [AnyHashable] {
-            // Using Xcode's TestPlans
+            // TestPlan configuration
             let newTestConfigurations = testConfigurations.map { testConfiguration in
                 if var newTestConfiguration = testConfiguration as? [String: AnyHashable],
                    let testTargets = newTestConfiguration[testTargetsKey] as? [AnyHashable] {
                     let newTestTargets = testTargets.map { testTarget in
-                        if var newTestTarget = testTarget as? [String: AnyHashable],
-                           var allEnvironmentVariables = newTestTarget[environmentVariablesKey] as? [String: AnyHashable] {
-                            allEnvironmentVariables[key] = isMuterRunningValue
-                            allEnvironmentVariables[isMuterRunningKey] = isMuterRunningValue
-
-                            newTestTarget[environmentVariablesKey] = allEnvironmentVariables
-
-                            return newTestTarget as AnyHashable
+                        if let newTestTarget = testTarget as? [String: AnyHashable] {
+                            return updateEnvironmentVariables(
+                                forConfiguration: newTestTarget,
+                                key: key
+                            ) as AnyHashable
                         }
                         return testTarget
                     }
@@ -41,21 +37,36 @@ struct XCTestRun: Equatable {
             }
 
             copy[testConfigurationsKey] = newTestConfigurations
-        } else { // Legacy Tests config
+        } else { 
+            // Legacy Tests configuration
             for (plistEntry, plistValue) in copy {
-                if var testConfiguration = plistValue as? [String: AnyHashable],
-                   testConfiguration.keys.contains(environmentVariablesKey),
-                   var allEnvironmentVariables = testConfiguration[environmentVariablesKey] as? [String: AnyHashable] {
-                    allEnvironmentVariables[key] = isMuterRunningValue
-                    allEnvironmentVariables[isMuterRunningKey] = isMuterRunningValue
-
-                    testConfiguration[environmentVariablesKey] = allEnvironmentVariables
-
-                    copy[plistEntry] = testConfiguration
+                if let testConfiguration = plistValue as? [String: AnyHashable] {
+                    copy[plistEntry] = updateEnvironmentVariables(
+                        forConfiguration: testConfiguration,
+                        key: key
+                    )
                 }
             }
         }
 
         return copy
+    }
+
+    private func updateEnvironmentVariables(
+        forConfiguration configuration: [String: AnyHashable],
+        key: String
+    ) -> [String: AnyHashable] {
+        let environmentVariablesKey = "EnvironmentVariables"
+        var configuration = configuration
+
+        if configuration.keys.contains(environmentVariablesKey),
+           var allEnvironmentVariables = configuration[environmentVariablesKey] as? [String: AnyHashable] {
+            allEnvironmentVariables[key] = isMuterRunningValue
+            allEnvironmentVariables[isMuterRunningKey] = isMuterRunningValue
+
+            configuration[environmentVariablesKey] = allEnvironmentVariables
+        }
+
+        return configuration
     }
 }
