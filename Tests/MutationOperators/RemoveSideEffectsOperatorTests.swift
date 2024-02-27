30efbb204dbc42e3d62ea438c891c6ac51fc5fad
Rafael Machado <raka.cdc@gmail.com>
2024-02-27 14:34:42 -0300

    wip fixing remove side effects

--- Tests/MutationOperators/RemoveSideEffectsOperatorTests.swift
+++ Tests/MutationOperators/RemoveSideEffectsOperatorTests.swift
@@ -32,7 +32,7 @@ final class RemoveSideEffectsOperatorTests: MuterTestCase {
         )
     }
 
-    func assertMutationPositions(
+    private func assertMutationPositions(
         _ actual: SchemataMutationMapping,
         _ expected: [MutationPosition],
         file: StaticString = #file,
@@ -54,5 +54,50 @@ final class RemoveSideEffectsOperatorTests: MuterTestCase {
         let rewriter = MuterRewriter(visitor.schemataMappings).rewrite(sourceWithSideEffects.code)
 
         AssertSnapshot(formatCode(rewriter.description))
+    }
+
+    func test_rewriter2() throws {
+        let source = try sourceCode(
+            """
+            static func validate(_ type: ParsableArguments.Type, parent: InputKey?) -> ParsableArgumentsValidatorError? {
+              let argumentKeys: [InputKey] = Mirror(reflecting: type.init())
+                .children
+                .compactMap { child in
+                  guard
+                    let codingKey = child.label,
+                    let _ = child.value as? ArgumentSetProvider
+                    else { return nil }
+
+                  // Property wrappers have underscore-prefixed names
+                  return InputKey(name: codingKey, parent: parent)
+              }
+              guard argumentKeys.count > 0 else {
+                return nil
+              }
+              do {
+                let _ = try type.init(from: Validator(argumentKeys: argumentKeys))
+                return InvalidDecoderError(type: type)
+              } catch let result as Validator.ValidationResult {
+                switch result {
+                case .missingCodingKeys(let keys):
+                  return MissingKeysError(missingCodingKeys: keys)
+                case .success:
+                  return nil
+                }
+              } catch {
+                fatalError("Unexpected validation error: error")
+              }
+            }
+            """
+        )
+        let visitor = RemoveSideEffectsOperator.Visitor(
+            sourceCodeInfo: .init(path: "", code: source)
+        )
+
+        visitor.walk(source)
+
+        let rewriter = MuterRewriter(visitor.schemataMappings).rewrite(source)
+
+        print(formatCode(rewriter.description))
     }
 }
