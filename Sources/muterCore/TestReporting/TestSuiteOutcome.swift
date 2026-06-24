2d795a0061563547b1b64691cab48cda58a6e312
pending-existence <293449359+pending-existence@users.noreply.github.com>
2026-06-25 01:03:04 +0200

    fix: detect Swift Testing failures by matching 'issue(s)' in test logs

--- Sources/muterCore/TestReporting/TestSuiteOutcome.swift
+++ Sources/muterCore/TestReporting/TestSuiteOutcome.swift
@@ -66,7 +66,7 @@ extension TestSuiteOutcome {
     }
 
     private static var testFailureRegEx: NSRegularExpression {
-        NSRegularExpression.regexWithPattern(#"test\(s\)? failed|with ([1-9][0-9]*) failure|with ([1-9][0-9]*) tests? failed"#)
+        NSRegularExpression.regexWithPattern(#"test\(s\)? failed|with ([1-9][0-9]*) (failure|issue)|with ([1-9][0-9]*) tests? failed"#)
     }
 
     private static func terminationStatusIsSuccess(_ terminationStatus: Int32) -> Bool {
